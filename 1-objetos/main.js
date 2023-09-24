88347d88368ea4162f5b283471bf1ab9d1d8a5a2
Lau Luraschi <loud.luraschi@gmail.com>
2023-09-24 20:31:09 +0200

    peras

--- 1-objetos/main.js
+++ 1-objetos/main.js
@@ -25,19 +25,21 @@ render(adalaber1);
 render(adalaber2);
 
 //TODO:Cesta de peras2 Crear una cesta de peras
-const basket ={
-    max:10,
-    min:0,
-    current:7,
-    initial:1,
-    add:current++,
-    take:current--,
-    reset:current===initial,
-}
-console.log(basket.max);
-console.log(basket.min);
-console.log(basket.current);
-console.log(basket.initial);
-console.log(basket.add);
-console.log(basket.take);
-console.log(basket.reset);
+const max=document.querySelector('.js-max');
+const min=document.querySelector('.js-min');
+const init=document.querySelector('.js-init');
+const current=document.querySelector('.js-current');
+
+const btnAdd=document.querySelector('.js-btn-add');
+const btnRest=document.querySelector('.js-btn-rest');
+const btnRst=document.querySelector('.js-btn-rst');
+
+// const basket ={
+//     max:10,
+//     min:0,
+//     current:7,
+//     initial:1,
+//     add:current++,
+//     rest:current--,
+//     reset:current===initial,
+// }
