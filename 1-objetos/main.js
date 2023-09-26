12fca6f3627eb0a4527e639fc700916ca7989be2
Lau Luraschi <loud.luraschi@gmail.com>
2023-09-26 17:28:07 +0200

    objetos

--- 1-objetos/main.js
+++ 1-objetos/main.js
@@ -91,3 +91,12 @@ user.age++;
 
 console.log(user);
 
+// TODO Ejercicio Extra. Investigando
+// Prepara un botón con un listener para loguear el objeto event en la consola. Investiga si la clave type que encontramos en él es una propiedad o un método.
+// Prepara un input de tipo texto, a recogerlo desde JavaScript y a imprimirlo en la consola con console.dir para investigarlo y comentar con la compañera estas propiedades: value, nodeNamey required.
+// Nota Cuando imprimimos un elemento en la consola con console.log vemos la etiqueta de HTML y podemos desplegarla para ver su contenido. Si queremos ver el objeto con sus propiedades y métodos tendremos que usar console.dir.
+const logBtn=document.querySelector('.js-btn-log');
+
+logBtn.addEventListener('click', handleLog);
+console.log(event);
+
