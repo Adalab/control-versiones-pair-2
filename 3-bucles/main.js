d6b03c8f40300f7b3cbf522df5c560bad100ed3c
Lau Luraschi <loud.luraschi@gmail.com>
2023-09-26 10:24:19 +0200

    bucles

---
+++ 3-bucles/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+for (let i = 1; i < 11; i++) {
+    console.log('Voy por la vuelta ' + i);
+  }
+
+
+
+let acc = 0;
+for (let i = 0; i < 10; i++) {
+  acc+=2;
+}
+console.log('El resultado es ' + acc);
